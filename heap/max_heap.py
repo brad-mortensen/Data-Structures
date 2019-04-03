b9b5904014020064ef5076177d133567694bb09b
Brad Mortensen <b.mort86@gmail.com>
2019-04-03 16:57:17 -0600

    adds some max heap stuff

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,32 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage)-1)
 
     def delete(self):
         pass
 
     def get_max(self):
-        return self.storage[0]
+        if self.storage:
+            return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        parent = index//2
+        if index <= 1:
+            return
+        elif self.storage[index] > self.storage[parent]:
+            self._swap(index, parent)
+            self._bubble_up(parent)
+
+    def _swap(self, i, j):
+        self.storage[i], self.storage[j] = self.storage[j], self.storage[i]
 
     def _sift_down(self, index):
         pass
+
+my_heap = Heap()
+my_heap.insert(4)
